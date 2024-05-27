bbb51c49a49a3da5ae6e16f525e978c17480c026
Tomasz Bajera <99763277+ImVertil@users.noreply.github.com>
2024-05-27 22:30:53 +0200

    Movement smoothing added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,12 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _sprintSpeed = 8f;
+    [SerializeField] private float _smoothTime = 0.2f;
     [SerializeField] private CharacterController _controller;
+
+    private Vector2 _currentVelocity;
+    private Vector2 _currentMovementVector;
 
 
     private void Start()
     {
-        _controller = GetComponent<CharacterController>();    
+
     }
 
     private void Update()
@@ -23,7 +27,8 @@ public class PlayerController : MonoBehaviour
     private void HandleMovement()
     {
         Vector2 inputVector = PlayerInputHandler.Instance.Move;
-        Vector3 movementVector = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+        _currentMovementVector = Vector2.SmoothDamp(_currentMovementVector, inputVector, ref _currentVelocity, _smoothTime);
+        Vector3 movementVector = new Vector3(_currentMovementVector.x, 0f, _currentMovementVector.y);
 
         _controller.Move(movementVector * (PlayerInputHandler.Instance.Sprint ? _sprintSpeed : _speed) * Time.deltaTime);
     }
